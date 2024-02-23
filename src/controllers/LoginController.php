2674c060d21bac6d721f50695be48d58f2742732
unknown <engs-weslleyzampier.camporeal.edu.br>
2024-02-22 22:13:13 -0300

    Aula dia 22-02

---
+++ src/controllers/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+ require_once "../chamadoReal/src/models/User.php";
+
+ class LoginController(){
+    private $users;
+
+    public function __construct($users){
+        $this->users=[
+            new User(1,"adm@teste.com.br",'1234',1);
+            new User(2,"user@teste.com.br",'1234',1);
+            new User(3,"jose@teste.com.br",'1234',2);
+        ]
+    }
+
+    public function autenticar($email, $password){
+        foreach($this->users as $user){
+            if(($user->email == $email) && ($user->password == $password)){
+                $_SESSION['autenticacao'] = 'SIM';
+                $_SESSION['id'] =  $user->id;
+                $_SESSION['profile_id'] = $user->profile_id;
+                header('Location: ../chamadoReal/src/view/home.php');
+                exit;
+            }
+        }
+
+        $_SESSION['autenticacao'] = "NAO";  
+        header('Location: index.php?login=erro')  
+    }
+ }
+?>
